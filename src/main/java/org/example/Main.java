f17c22edc88ba4be8e9d3ffa438b381652ba9fa0
Andrew <telepnev.1997@mail.ru>
2024-02-09 17:59:36 +0700

    add ignoring .idea

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.example.config.ProjectConfig;
+import org.example.domain.Car;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Car p = context.getBean(Car.class);
+        p.go();
+    }
+}
